392fa721f90e120c7cafa822edae9e9a0947a010
Matheus Lopes <matheuslopes.developer@gmail.com>
2017-08-30 00:10:26 -0300

    Olá React

---
+++ README.md
@@ -0,0 +1,1 @@
+# React-Basics é um projeto para estudo dos conceitos básicos do React JS
---
+++ ex/index.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import ReatDOM from 'react-dom'
+
+ReactDOM.render(<h1>Olá React</h1>, document.getElementById('app'))
